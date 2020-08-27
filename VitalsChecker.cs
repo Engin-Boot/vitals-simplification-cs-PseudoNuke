57d82b5f7d67ee8794467f012d3e691135f8610d
PseudoNuke <thilakashwin10@gmail.com>
2020-08-27 21:33:15 +0530

    Update VitalsChecker.cs

--- VitalsChecker.cs
+++ VitalsChecker.cs
@@ -4,7 +4,7 @@ namespace Vitals{
     
     class VitalsChecker
     {
-        static bool vitalsAreOk(float bpm, float spo2, float respRate) {
+        public static bool vitalsAreOk(float bpm, float spo2, float respRate) {
             var bpm_check = (bpm < 70 || bpm > 150);
             var spo2_check = (spo2 < 90);
             var respRate_check = (respRate < 30 || respRate > 95);

af800066f9fd491e8ab5b05d77db07759f7bc11f
PseudoNuke <thilakashwin10@gmail.com>
2020-08-27 21:37:00 +0530

    Update VitalsChecker.cs

--- VitalsChecker.cs
+++ VitalsChecker.cs
@@ -1,16 +1,47 @@
 using System;
-using System.Diagnostics;
-namespace Vitals{
-    
-    class VitalsChecker
-    {
-        public static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-            var bpm_check = (bpm < 70 || bpm > 150);
-            var spo2_check = (spo2 < 90);
-            var respRate_check = (respRate < 30 || respRate > 95);
-            if( bpm_check || spo2_check || respRate_check )
-                return false;
+
+class VitalsChecker
+{
+     public static bool vitalsAreOk(float bpm, float spo2, float respRate) {
+       return !(checkbpm(bpm)||checkspo2(spo2)||checkrespRate(respRate));
+    }
+    static bool checkbpm(float bpm)
+    { 
+         if(bpm < 70)
+         {  
+             Console.WriteLine("Beat per heart is less than 70");
+             return true;
+         } 
+         if( bpm > 150)
+    {   Console.WriteLine("Beat per heart is greater than 150");
+        return true;
+        
+    }
+    return false;
+
+    }
+    static bool checkspo2(float spo2)
+    { 
+         if(spo2 < 90)
+         { Console.WriteLine("Oxygen Saturation level is less than 90");
+         return true;
+        
+         }
+         return false;
+        
+    }static bool checkrespRate(float respRate)
+    { 
+         if(respRate < 30)
+         {
+              Console.WriteLine("Respiration Rate level is less than 30");
             return true;
-        }
-    }   
+         } 
+         if(respRate > 95)
+         {
+              Console.WriteLine("Respiration Rate  level is greater than 95");
+              return true;
+         }
+         return false;
+            
+    }
 }

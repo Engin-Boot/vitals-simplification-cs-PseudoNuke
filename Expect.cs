47e80f4aded153ef551e5128a0e2593d8163517a
PseudoNuke <thilakashwin10@gmail.com>
2020-08-27 20:49:06 +0530

    Create Expect.cs

--- Expect.cs
+++ Expect.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Diagnostics;
-class ExpectFalse
+class Expect
 {
     static void ExpectFalse(bool expression) {
         if(expression) {
             Console.WriteLine("Expected false, but got true");
             Environment.Exit(1);
-        }
+        }   
     }
+    static void ExceptTrue(bool expression) {
+         if(!expression) {
+              Console.WriteLine("Expected true, but got false");
+              Environment.Exit(1);
+          }
+      } 
  } 

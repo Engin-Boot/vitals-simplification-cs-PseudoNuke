dc80b87e4acfd8a4e4c2653f6bf34990937ef83b
PseudoNuke <thilakashwin10@gmail.com>
2020-09-07 13:12:50 +0530

    Update Expect.cs

--- Expect.cs
+++ Expect.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Diagnostics;
-class Expect
+public class Expect
 {
-    static void ExpectFalse(bool expression) {
+    public static void ExpectFalse(bool expression) {
         if(expression) {
             Console.WriteLine("Expected false, but got true");
             Environment.Exit(1);
         }   
     }
-    static void ExceptTrue(bool expression) {
+    public static void ExceptTrue(bool expression) {
          if(!expression) {
               Console.WriteLine("Expected true, but got false");
               Environment.Exit(1);
